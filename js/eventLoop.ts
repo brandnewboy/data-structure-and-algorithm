1cbfd692359c1780bf28f7c2b4a4b7a63865b885
lizhang <2475934003@qq.com>
2023-03-04 13:34:23 +0800

    example_eventLoop

---
+++ js/eventLoop.ts
@@ -0,0 +1,28 @@
+new Promise<void>(function (resolve, reject) {
+  // console.log(1)
+  setTimeout(() => {
+    console.log(1)
+  }, 0)
+  resolve()
+})
+  .then(function () {
+    console.log(2)
+  })
+  .then(function () {
+    console.log(3)
+  })
+
+setTimeout(function () {
+  console.log(4)
+}, 0)
+
+new Promise<void>(function (resolve, reject) {
+  console.log(5)
+  resolve()
+}).then(function () {
+  console.log(6)
+})
+
+console.log(7)
+
+// 5 7 2 6 3 1 4
